6cd4029a29da16c4d2069266634489bf7eeeaef2
Karen Mirzayan <k_mirzayan@kbtu.kz>
2023-01-31 19:14:57 +0600

    Added Lab2 and moved !!!NumOfExamples to Lab1

---
+++ Lab2/15.py
@@ -0,0 +1,7 @@
+# and 	Returns True if both statements are true	x < 5 and  x < 10	
+# or	Returns True if one of the statements is true	x < 5 or x < 4	
+# not	Reverse the result, returns False if the result is true	not(x < 5 and x < 10)
+# is 	Returns True if both variables are the same object	x is y	
+# is not	Returns True if both variables are not the same object
+# in 	Returns True if a sequence with the specified value is present in the object	x in y	
+# not in	Returns True if a sequence with the specified value is not present in the object
