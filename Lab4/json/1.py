239c95c80c9f2ca008db21924e0b2b348a2d07dd
Karen Mirzayan <k_mirzayan@kbtu.kz>
2023-02-25 14:05:02 +0600

    Optimization

--- Lab4/json/1.py
+++ Lab4/json/1.py
@@ -1,10 +1,10 @@
 import json
 with open('Lab4\json\sample-data.json', 'r') as smp:
     s = json.load(smp)
-    print('Interface Status')
-    print("================================================================================")
-    print('DN                                                 Description           Speed     MTU  ')
-    print('-------------------------------------------------- --------------------  -------  ------')
+    print('''Interface Status
+================================================================================
+DN                                                 Description           Speed    MTU  
+-------------------------------------------------- --------------------  -------  ------''')
     for i in s['imdata']:
         print("{:50} {:20}  {:7}  {:6}".format(i['l1PhysIf']['attributes']['dn'], i['l1PhysIf']['attributes']['descr'], i['l1PhysIf']['attributes']['speed'], i['l1PhysIf']['attributes']['mtu']))
 
